5b9978d95bff1b0ee49ca18585fb41be2af98972
Max Zinovik <zinovik@gmail.com>
2019-12-28 09:55:41 +0300

    update modules, add file remobing, tslint to eslint

--- src/file-system/FileSystemService.service.ts
+++ src/file-system/FileSystemService.service.ts
@@ -5,6 +5,6 @@ import { IFileSystemService } from './IFileSystemService.interface';
 
 export class FileSystemService implements IFileSystemService {
   async removeFile(path: string): Promise<void> {
-    //
+    await promisify(fs.unlink)(path);
   }
 }
